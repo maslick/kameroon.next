34ade0a604c66af29bca5f47faa84e7bb03a1854
Pavel Maslov <pavel.maslov@axfood.se>
2025-01-25 21:03:43 +0100

    move back button

--- app/scan/page.js
+++ app/scan/page.js
@@ -61,23 +61,6 @@ export default function ScanCodePage() {
   return (
     <div
       className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <a href="/"
-         className="fixed top-8 left-3 rounded-full w-14 h-14 flex items-center justify-center bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] shadow-lg transition-all z-50">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M19 12H5"/>
-          <path d="M12 19l-7-7 7-7"/>
-        </svg>
-      </a>
 
       <main className="flex flex-col gap-8 row-start-2 sm:items-center">
         <div className="font-[family-name:var(--font-geist-mono)]">
@@ -98,6 +81,23 @@ export default function ScanCodePage() {
               Stop
             </button>
           }
+          <a href="/"
+             className="rounded-full w-12 h-12 flex items-center justify-center bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] shadow-lg transition-all z-50">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 6L6 18"/>
+              <path d="M6 6l12 12"/>
+            </svg>
+          </a>
         </div>
       </main>
     </div>

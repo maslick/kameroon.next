d5a576f8c0cca95053b263739abd5a863f3e1940
Pavel Maslov <pavel.maslov@axfood.se>
2025-01-25 20:47:34 +0100

    make the button float in the bottom right corner

--- app/page.js
+++ app/page.js
@@ -7,15 +7,13 @@ export default function Home() {
         <div className="font-[family-name:var(--font-geist-mono)]">
           Welcome to Kameroon.Next
         </div>
-        <div className="flex gap-4 items-center flex-col sm:flex-row content-center">
-          <a href="/scan" className="rounded-full w-14 h-14 flex items-center justify-center bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] shadow-lg transition-all">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
-              <circle cx="12" cy="13" r="4"/>
-            </svg>
-          </a>
-        </div>
       </main>
+      <a href="/scan" className="fixed bottom-8 right-8 rounded-full w-14 h-14 flex items-center justify-center bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] shadow-lg transition-all">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
+          <circle cx="12" cy="13" r="4"/>
+        </svg>
+      </a>
     </div>
   );
 }

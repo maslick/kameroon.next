a074a926cd341766680b90149f778582d67aece3
Pavel Maslov <pavel.maslov@axfood.se>
2025-02-11 15:03:04 +0100

    fixing styling

--- app/settings/page.js
+++ app/settings/page.js
@@ -85,7 +85,7 @@ export default function SettingsPage() {
                 {settings.crossHair && (
                   <div className="flex items-center justify-center pt-8">
                     <select
-                      className="w-[200px] bg-gray-50 border border-gray-300 text-gray-900 text-pretty rounded-lg focus:ring-green-500 focus:border-green-500 p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 appearance-none "
+                      className="w-[200px] bg-gray-50 border border-gray-300 text-gray-900 text-pretty rounded-lg p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white appearance-none focus:outline-none"
                       value={settings.crossHairStyle}
                       onChange={(e) => updateSettings('crossHairStyle', e.target.value)}
                     >

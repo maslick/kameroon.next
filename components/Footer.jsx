6c7ed37d0288875804c7652643976f4312d83f33
Pavel Maslov <pavel.maslov@axfood.se>
2025-02-11 15:44:14 +0100

    remove unused code

--- components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <div>This is a sample Footer component</div>
-    </footer>
-  );
-};

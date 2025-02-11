2e3e6f6b1ff6ee0ea0c98168e9e1b1bd81c270d5
Pavel Maslov <pavel.maslov@axfood.se>
2025-02-11 10:54:45 +0100

    configure next.js export mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  output: "export",
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        worker_threads: false
+      };
+    }
+
+    // Add this configuration for Web Workers
+    config.output = {
+      ...config.output,
+      globalObject: 'self'
+    };
+
+    return config;
+  }
 };
 
 export default nextConfig;
